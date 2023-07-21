e1c7dd6679a2cd86018d994cadfb9e0a56c01d56
Eduardo Alvarado <ealvara73@gmail.com>
2023-07-21 14:14:05 -0500

    Create header component

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import icon from './assets/memory-64.png';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={icon} alt="" />
+      <h1 className="title">Memory Card Game</h1>
+    </div>
+  )
+}
+
+export default Header;
