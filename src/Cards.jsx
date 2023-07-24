e767ce9717abc5fb508acf3892ae47fb7b7b91c3
Eduardo Alvarado <ealvara73@gmail.com>
2023-07-24 15:18:32 -0500

    Add new levels when completeinga level

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,34 +1,51 @@
 import { useEffect, useState } from "react";
 import Card from "./Card";
 import arrayShuffle from 'array-shuffle';
-import data from './data.json';
 
 
-const Cards = () => {
-  let pokemonArr = data[0];
+const Cards = (props) => {
+  const { level, nextLevel, usedPokemon } = { ...props };
   const api = 'https://pokeapi.co/api/v2/pokemon/';
 
   const [pokemon, setPokemon] = useState({
     isLoaded: false,
-    pokemonData: [],
+    pokemonIds: [],
+    pokemonData: [],  
   });
+
+  const checkLevel = () => {
+    if (pokemon.pokemonData.every((item) => item.isclicked)) {
+      nextLevel(pokemon.pokemonIds);
+    }
+  }
 
   const handleClick = (id) => {
     const newArr = pokemon.pokemonData.map((item) => {
       if (item.id === id) {
-        if (item.isclicked) console.log(`${item.name} was clicked twice`);
-        else item.isclicked = true;
+        item.isclicked = true;
       }
       return item;
     });
 
+    checkLevel();
+
     setPokemon({
       isLoaded: true,
+      pokemonIds: pokemon.pokemonIds,
       pokemonData: arrayShuffle(newArr),
-    }); 
+    });
   }
 
   useEffect(() => {
+    const pokemonArr = [];
+    let id = undefined;
+    for (let i = 0; i < level; i += 1) {
+      do {
+        id = Math.floor(Math.random() * 150) + 1;
+      }
+      while (pokemonArr.includes(id) || usedPokemon.includes(id));
+      pokemonArr.push(id);
+    }
       const promises = pokemonArr.map(async (pokemonName) => {
         const response = await fetch(api + pokemonName);
         const json = await response.json();
@@ -44,11 +61,11 @@ const Cards = () => {
       Promise.all(promises).then((arr) => {
         setPokemon({
           isLoaded: true,
+          pokemonIds: pokemonArr,
           pokemonData: arr,
         });
-      })
-  }, []);
-
+      }).catch((e) => console.log(e));
+  }, [level]);
   const cardsArr = pokemon.pokemonData.map((item) => {
   return <Card key={item.id} id={item.id} name={item.name} img={item.img} handleClick={handleClick} />
 });
--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import Cards from "./Cards";
 
 const MainContent = () => {
+  const [level, setLevel] = useState([4, 6, 8, 10, 12, 14, 16]);
+  const [usedPokemon, setUsedPokemon] = useState([]);
+
+  const nextLevel = (pokemonIds) => {
+    console.log('next level was triggered');
+    const newLevel = level.slice(1);
+    const tmpArr = usedPokemon.concat(pokemonIds);
+    setLevel(newLevel);
+    setUsedPokemon(tmpArr);
+  }
+
   return (
     <section className="main-content">
-      <Cards />
+      <Cards level={level[0]} nextLevel={nextLevel} usedPokemon={usedPokemon} />
     </section>
   )
 }
