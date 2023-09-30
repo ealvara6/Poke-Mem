7a9898d0b3d84c62ca7d640cbadf755f07636044
Eduardo Alvarado <ealvara73@gmail.com>
2023-09-29 22:18:27 -0500

    Add additional levels

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Footer from './Footer';
 import StartGen from './StartGen';
 
 function App() {
-  const levels = [4, 5, 6, 7, 8, 9, 10];
+  const levels = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
   const [level, setLevel] = useState({
     levels,
     currentLevel: 1,
